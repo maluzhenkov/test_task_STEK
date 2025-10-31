57bfaf5681613c02d0e0e89f4a287547df19d194
Nikolay Maluzhenkov <Kolyan57024@mail.ru>
2025-11-01 00:19:20 +0300

    remove comm

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  // Для GitHub Pages: base должен быть '/repository-name/'
-  // Если репозиторий называется test_task_STEK, оставьте '/test_task_STEK/'
-  // Если репозиторий имеет другое имя, измените base соответственно
-  base: process.env.NODE_ENV === 'production' 
-    ? (process.env.VITE_BASE_URL || '/test_task_STEK/') 
-    : '/',
-})
+  base:
+    process.env.NODE_ENV === 'production'
+      ? process.env.VITE_BASE_URL || '/test_task_STEK/'
+      : '/',
+});
